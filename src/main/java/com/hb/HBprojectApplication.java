5696f6171fd3a7020794c720156174ae618d6491
pavan nagargoje <pavannagargoje3236@gmail.com>
2021-08-04 11:09:33 +0530

    Main class modified

--- src/main/java/com/hb/HBprojectApplication.java
+++ src/main/java/com/hb/HBprojectApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class})
 public class HBprojectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HBprojectApplication.class, args);
+		ConfigurableApplicationContext context= SpringApplication.run(HBprojectApplication.class, args);
+		
+		context.close();
 	}
 
 }
